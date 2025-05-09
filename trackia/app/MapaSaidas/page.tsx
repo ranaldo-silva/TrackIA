a9f6ee72c4e1fc916b8ed0445f11f65031290f1b
EquipeFiapProjeto <rm559210@fiap.com.br>
2025-05-09 16:07:09 -0300

    correção erro mapa de saida, estava sem "use client"

--- trackia/app/MapaSaidas/page.tsx
+++ trackia/app/MapaSaidas/page.tsx
@@ -1,15 +1,4 @@
-import Link from "next/link";
-
-export default function MapaSaidas(){
-    return(
-        <div>
-            <div className="absolute top-4 right-4">
-                <Link href="/"className="bg-[#740000] hover:bg-[#970000] text-white py-2 px-4 rounded">Início</Link>
-            </div>
-            <p>Ajuste com API</p>
-        </div>
-    )
-}"use client";
+"use client";
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
